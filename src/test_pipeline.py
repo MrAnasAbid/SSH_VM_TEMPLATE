c110386d4fbfb1098ad4be48f13353ffd0ccd131
MrAnasAbid <m.abid.anas@gmail.com>
2024-11-18 23:31:11 +0100

    Slight modifications

--- src/test_pipeline.py
+++
@@ -1,6 +0,0 @@
-from pandas import DataFrame
-
-if __name__ == '__main__':
-    # Load data
-    data = DataFrame({'a': [1, 2, 3], 'b': [4, 5, 6]})
-    print(data)
---
+++ tests/health_check.py
@@ -0,0 +1,6 @@
+from pandas import DataFrame
+
+# Check health of the pipeline, return 200 if everything is fine
+if __name__ == '__main__':
+    print('Pipeline is healthy')
+    print('200')
